a19aec8791a342b0417c0600913c4469733dc000
Sean Butner <sean.butner@gmail.com>
2017-06-12 12:45:18 -0500

    Build skeleton of algorithm and initial helper functions

---
+++ get_step_dates.py
@@ -0,0 +1,37 @@
+import os
+import time
+
+import secret
+
+#for each folder in TLG Reports
+        #validate folder is a customer folder
+            #valid folders have iReview, Final Review and Final Invoice Review folders
+        #for each month in customer folder
+                #grab the step 1 date: iReview folder creation
+                    #exclude the iReview folders made on run of step 3
+                #assume step 2 same day as step 3 
+                #grab the step 3 date: Final Invoice Review file ctime OR iReview folder parsing
+                #grab the step 4 date: Final Review file ctime
+        #return the CUSTOMER_NAME|MONTH|STEP|DATE collected above
+#return a table of CUSTOMER_NAME|MONTH|STEP|DATE
+#print table as csv
+
+
+
+
+
+
+def getTime(path, mode="c"):
+    if (mode = "c" or mode = "created"):
+        date = time.gmtime(os.path.getctime(path))
+    else:
+        date = time.gmtime(os.path.getmtime(path))
+    if (date[3] < 7):
+        day = date[2] - 1
+    else:
+        day = date[2]
+    strDate = str(date[1])+'/'+str(day)+'/'+str(date[0])
+    return strDate
+
+if __name__ == "__main__":
+    pass
