8f91545f9c13517a80a6d24d8b0f799187ff601d
Sean Butner <sean.butner@gmail.com>
2017-06-12 23:30:40 -0500

    Add the remainder of the (untested) compilation functions

--- get_step_dates.py
+++ get_step_dates.py
@@ -1,5 +1,6 @@
 import os
 import time
+import pandas as pd
 
 import secret
 
@@ -18,19 +19,55 @@ import secret
 
 
 def walkThroughFolders(root):
-#walking through at first
-#just adding up and returning number
-#of valid dirs
-    validDirs = 0
+    stageDatesResults = pd.DataFrame()
     for path in os.listdir(root):
         if validateFolder(root+path):
-            validDirs += 1
-    return validDirs
+            stageDatesResults = stageDatesResults.append(compileCustomer(path))
+    return stageDatesResults
 
 def validateFolder(path):
     if os.path.isdir(path):
-        return VALID_SUBFOLDER in os.listdir(path)    
+        return (secret.VALID_SUBFOLDER in os.listdir(path))
 
+def compileCustomer(path):
+    customerName = path
+    path = secret.ROOT_PATH + customerName
+    customerResults = pd.DataFrame({'customerName' : customerName,
+                                    'firstStage' : getFirstStage(path),
+                                    'thirdStage' : getThirdStage(path),
+                                    'fourthStage' : getFourthStage(path)})
+    return customerResults
+
+def getFirstStage(path):
+    path = path+'prior months/'
+    dates = [file for file in os.listdir(path) if isReviewSheet(file, "First")]
+    dates = {(file[0:5],getTime(path+file)) for file in dates}
+    return dates
+
+def getThirdStage(path):
+    path = path+'Final Invoice Review'
+    dates = {(file[0:5],getTime(file) for file in os.listdir(path) if isReviewSheet(file, "Third")}
+    return dates    
+
+def getFourthStage(path):
+    path = path+'ireview'
+    dates = {(folder[0:5],parseDate(folder)) for folder in os.listdir(path)}
+    return dates
+
+def parseDate(folder):
+    month, day, year, time = (folder[8:10], folder[11:13], folder[14:18],folder[20:])
+        if (int(time) < 600):
+                day = int(day) - 1
+    dateString = month+'/'+str(day)+'/'+year
+    return dateString
+
+def isReviewSheet(path, stage):
+    if stage == "First":
+        return '.xls' in path and ' - Final' not in path
+    else if stage == "Third":
+        return '.xls' in path and ' - Final Invoice Review' in path        
+    else if stage == "Fourth":
+        return '.xls' in path and ' - Final' in path
 def getTime(path, mode="c"):
     if (mode == "c" or mode == "created"):
         date = time.gmtime(os.path.getctime(path))
@@ -44,4 +81,4 @@ def getTime(path, mode="c"):
     return strDate
 
 if __name__ == "__main__":
-    pass
+    walkThroughFolders(secret.ROOT_PATH).to_csv()
